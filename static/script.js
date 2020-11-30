4c5493747c0bf47e2964368b475cfe201c5a2f69
onanalee <69330162+onanalee@users.noreply.github.com>
2020-12-01 07:22:45 +0900

    CSS and JS properly connected

---
+++ static/script.js
@@ -0,0 +1,16 @@
+function generatePassword(length = 12) {
+    $('#password').empty();
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+    var all = uppercase + lowercase + numbers + symbols;
+    var password = '';
+    for (var index = 0; index < length; index++) {
+        var character = Math.floor(Math.random() * all.length);
+        password += all.substring(character, character + 1);
+    }
+    // return password;
+
+    $('#password').append(password);
+}
