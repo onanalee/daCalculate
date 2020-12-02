0a1aef6e020e8076f40458ec033a8da0567a1b7c
onanalee <69330162+onanalee@users.noreply.github.com>
2020-12-02 14:58:10 +0900

    show/hide functions added

--- static/script.js
+++ static/script.js
@@ -1,19 +1,13 @@
-function generatePassword(length = 12) {
-    $('#password').empty();
-    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    var numbers = '0123456789';
-    var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
-    var all = uppercase + lowercase + numbers + symbols;
-    var password = '';
-    for (var index = 0; index < length; index++) {
-        var character = Math.floor(Math.random() * all.length);
-        password += all.substring(character, character + 1);
-    }
-    // return password;
-
-    $('#password').append(password);
+function begin(){
+    $('.length').hide();
+    $('.weight').hide();
+    $('.area').hide();
+    $('.volume').hide();
+    $('.temperature').hide();
+    $('.bytes').hide();
+    $('.pressure').hide();
 }
+
 
 $(function() {
 
@@ -32,3 +26,92 @@ $(function() {
 			} else { $(this).addClass('expand'); }
 		})
 })
+
+function length() {
+    $('.length').show();
+    $('.weight').hide();
+    $('.area').hide();
+    $('.volume').hide();
+    $('.temperature').hide();
+    $('.bytes').hide();
+    $('.pressure').hide();
+
+}
+function weight() {
+    $('.weight').show();
+    $('.length').hide();
+    $('.area').hide();
+    $('.volume').hide();
+    $('.temperature').hide();
+    $('.bytes').hide();
+    $('.pressure').hide();
+}
+function area() {
+    $('.area').show();
+    $('.length').hide();
+    $('.weight').hide();
+    $('.volume').hide();
+    $('.temperature').hide();
+    $('.bytes').hide();
+    $('.pressure').hide();
+}
+function volume() {
+    $('.volume').show();
+    $('.length').hide();
+    $('.weight').hide();
+    $('.area').hide();
+    $('.temperature').hide();
+    $('.bytes').hide();
+    $('.pressure').hide();
+}
+function temperature() {
+    $('.temperature').show();
+    $('.length').hide();
+    $('.weight').hide();
+    $('.area').hide();
+    $('.volume').hide();
+    $('.bytes').hide();
+    $('.pressure').hide();
+}
+function bytes() {
+    $('.bytes').show();
+    $('.length').hide();
+    $('.weight').hide();
+    $('.area').hide();
+    $('.volume').hide();
+    $('.temperature').hide();
+    $('.pressure').hide();
+}
+function pressure() {
+    $('.pressure').show();
+    $('.length').hide();
+    $('.weight').hide();
+    $('.area').hide();
+    $('.volume').hide();
+    $('.temperature').hide();
+    $('.bytes').hide();
+}
+
+
+
+
+
+
+
+
+function generatePassword(length = 12) {
+    $('#password').empty();
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+    var all = uppercase + lowercase + numbers + symbols;
+    var password = '';
+    for (var index = 0; index < length; index++) {
+        var character = Math.floor(Math.random() * all.length);
+        password += all.substring(character, character + 1);
+    }
+    // return password;
+
+    $('#password').append(password);
+}
