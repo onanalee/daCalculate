17f413b2dc9af76f599a65d0ef9506d5ca0ff8e0
onanalee <69330162+onanalee@users.noreply.github.com>
2020-12-04 13:49:18 +0900

    temperature done

--- static/temperature.js
+++ static/temperature.js
@@ -1,3 +1,51 @@
-function convertTemperature(){
+function convertTemperature() {
+    let unitA = $('#temperatureUnitA option:selected').val();
+    let unitB = $('#temperatureUnitB option:selected').val();
+
+    if (unitA === '(°C) Celsius' && unitB === '(°C) Celsius') {
+        let before = $('#temperatureBefore').val();
+        let after = before * 1;
+        $('#temperatureAfter').val(after);
+    }
+    if (unitA === '(°C) Celsius' && unitB === '(°F) Fahrenheit') {
+        let before = $('#temperatureBefore').val();
+        let after = before * (9/5) + 32;
+        $('#temperatureAfter').val(after);
+    }
+    if (unitA === '(°C) Celsius' && unitB === '(K) Kelvin') {
+        let before = $('#temperatureBefore').val();
+        let after = Number(before) + (273.15);
+        $('#temperatureAfter').val(after);
+    }
+    if (unitA === '(°F) Fahrenheit' && unitB === '(°C) Celsius') {
+        let before = $('#temperatureBefore').val();
+        let after = (Number(before)-32) * (5/9);
+        $('#temperatureAfter').val(after);
+    }
+    if (unitA === '(°F) Fahrenheit' && unitB === '(°F) Fahrenheit') {
+        let before = $('#temperatureBefore').val();
+        let after = before * 1;
+        $('#temperatureAfter').val(after);
+    }
+    if (unitA === '(°F) Fahrenheit' && unitB === '(K) Kelvin') {
+        let before = $('#temperatureBefore').val();
+        let after = ((Number(before)-32) * (5/9)) + 273.15;
+        $('#temperatureAfter').val(after);
+    }
+    if (unitA === '(K) Kelvin' && unitB === '(°C) Celsius') {
+        let before = $('#temperatureBefore').val();
+        let after = Number(before) - 273.15;
+        $('#temperatureAfter').val(after);
+    }
+    if (unitA === '(K) Kelvin' && unitB === '(°F) Fahrenheit') {
+        let before = $('#temperatureBefore').val();
+        let after = (Number(before) - 273.15) * (9/5) + 32;
+        $('#temperatureAfter').val(after);
+    }
+    if (unitA === '(K) Kelvin' && unitB === '(K) Kelvin') {
+        let before = $('#temperatureBefore').val();
+        let after = before * 1;
+        $('#temperatureAfter').val(after);
+    }
 
 }
