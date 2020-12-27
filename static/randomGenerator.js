d4cd055712da4c54e31d0a541820817fcdc3f4f5
onanalee <69330162+onanalee@users.noreply.github.com>
2020-12-27 14:11:32 +0900

    calculator added

--- static/randomGenerator.js
+++ static/randomGenerator.js
@@ -6,10 +6,7 @@ function randomNumber() {
     let result = Math.floor(Math.random() * Number(valueMax - valueMin + 1)) + Number(valueMin);
     $('#randomNum').append(result);
 }
-// function hello() {
-//   generatePassword();
-//   window.setInterval("generatePassword()", 500);
-// }
+
 function generatePassword() {
     $('#password').empty();
     let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -30,7 +27,6 @@ function generatePassword() {
     let isNumber = document.getElementById("number");
     let isUpper = document.getElementById("upper");
     let isSpecial = document.getElementById("special");
-
 
     if (isNumber.checked === true && isUpper.checked === true && isSpecial.checked === true) {
         for (let index = 0; index < length; index++) {
@@ -68,38 +64,29 @@ function generatePassword() {
             password += upperSymbols.substring(character, character + 1);
         }
     }
-    console.log(password);
     $('#password').html(password);
 }
-
 
 function generateLotto() {
     //get random numbers
     var numbers = [];
     var ball = document.getElementsByClassName("ball");
-
     while (numbers.length < 6) {
-
         var random = Math.floor(Math.random() * 45) + 1;
-
         if (numbers.indexOf(random) == -1) {
             numbers.push(random);
         }
     }
 
     //sort numbers in array
-
     numbers.sort(function (a, b) {
         return a - b;
     });
 
     //color balls
-
     for (var i = 0; i < ball.length; i++) {
-
         ball[i].style.backgroundColor = "white";
         ball[i].style.color = "#3d3c3a";
-
         for (var j = 0; j < numbers.length; j++) {
             if (numbers[j] == parseInt(ball[i].innerHTML)) {
                 ball[i].style.backgroundColor = "#fa8b60";
@@ -109,18 +96,14 @@ function generateLotto() {
     }
 
     //add numbers to history
-
     var history = [];
     var historyDiv = document.getElementById("history");
     var para = document.createElement("p");
     history.push(numbers.join(", "));
 
     for (var k in history) {
-
         var node = document.createTextNode(history[k]);
         para.appendChild(node);
         historyDiv.appendChild(para);
-
     }
-
 }
